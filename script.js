45b095762efbc453b69fbd24b74172cc3b6e21b4
Jmarkus <Jmarkus.fowler@gmail.com>
2024-11-10 12:20:00 -0500

    Laying input ground work

---
+++ script.js
@@ -0,0 +1,8 @@
+let zipCodes = []
+const submitBtn = document.getElementById("submit-btn")
+const inputEl = document.getElementById("zipcode")
+const zipcodeFromLocalStorage = JSON.parse( localStorage.getItem("zipCodes"))
+
+function store(zipcode) {
+
+}
