81ae5b6495a9926517eee2e914a1d37c005f8dbb
Jmarkus <Jmarkus.fowler@gmail.com>
2024-11-10 12:44:53 -0500

    Setting up local Storage for User input to be captured

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 let zipCodes = []
 const submitBtn = document.getElementById("submit-btn")
 const inputEl = document.getElementById("zipcode")
+const zipStore = document.getElementById("storedzip")
 const zipcodeFromLocalStorage = JSON.parse( localStorage.getItem("zipCodes"))
 
-function store(zipcode) {
-
+if (zipcodeFromLocalStorage) {
+    zipCodes = zipcodeFromLocalStorage
+    store(zipCodes)
 }
+
+function store(zipcode) {
+    let zipItems = ""
+    for (let i = 0; i < zipcode.length; i++) {
+        zipItems += `
+        <li>
+            <a target='_blank' href='${zipcode[i]}'>
+                ${zipcode[i]}
+            </a>    
+        </li>
+    `
+}
+zipStore.innerHTML = zipItems
+}
+
+submitBtn.addEventListener("click", function() {
+    zipCodes.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("zipCodes", JSON.stringify(zipCodes))
+    store(zipCodes)
+} )
