dd359ce3d9db488c1aaafd180cb0418d1c2e9d99
Jmarkus <Jmarkus.fowler@gmail.com>
2024-12-03 20:45:16 -0500

    Passing Tests

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    let zipCodes = [];
+let zipCodes = [];
 const submitBtn = document.getElementById("submit-btn");
 const inputEl = document.getElementById("zipcode");
 const zipStore = document.getElementById("storedzip");
@@ -24,17 +24,16 @@ function store(zipcode) {
 }
 
 submitBtn?.addEventListener("click", function () {
-
-    const zipcode = inputEl.value; 
-    zipCodes.push(zipcode);
+  const zipcode = inputEl.value;
+  zipCodes.push(zipcode);
 
   getCoordinates(zipcode)
     .then((coords) => {
       if (coords) {
         zipStore.innerText = `Lat: ${coords.latitude}, Lon: ${coords.longitude}`;
-        getWeather(coords).then((weather) => { 
-            console.log(weather)
-        })
+        getWeather(coords).then((weather) => {
+          console.log(weather);
+        });
       } else {
         zipStore.innerText = "No valid coordinates found.";
       }
@@ -77,22 +76,48 @@ async function getCoordinates(zipcode) {
   }
 }
 async function getWeather(coords) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords.latitude}&longitude=${coords.longitude}&current=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`;
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const resp_object = await response.json();
-        console.log(resp_object)
-        return resp_object
-    } catch (error) {
-      console.error(error.message);
-      throw error;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords.latitude}&longitude=${coords.longitude}&current=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+
+    const resp_object = await response.json();
+    console.log(resp_object);
+    return resp_object;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
-  function calculateBuggyRespone(temperature, weather_code) {
-    return 'Kind of Hot for me! Maybe a short walk or laying down outside.'
+}
+function calculateBuggyRespone(temperature, weather_code) {
+  if (85 >= temperature && temperature >= 76) {
+    return "Kind of Hot for me! Maybe a short walk or laying down outside.";
   }
-  module.exports = calculateBuggyRespone;
+  else if (100 >= temperature && temperature >= 86) {
+    return 'It is too hot for me. My paws would get burnt by the pavement, and I might get heat stroke if I am out to long. Water and shade help.'
+  }
+  else if (75 >= temperature && temperature >= 66) {
+    return "It is nice, but I sure don't want it to get any warmer! Maybe a rest break in 15 to 20."
+  }
+  else if (65 >= temperature && temperature >= 50) {
+    return 'Perfect day for a long walk with lots of smells, pets, and treats!!!'
+  }
+  else if (49 >= temperature && temperature >= 30) {
+    return 'Kind of Cold for me! Maybe a medium walk, or a jacket. But we should definitely go get coffee!'
+  }
+  else if (29 >= temperature && temperature >= 15) {
+    return "If it isn't wet out I should be okay for a medium walk. But if it is wet out my paws won't last more than 5 to 10 minutes."
+  }
+  else if (14 >= temperature && temperature >= 0) {
+    return "I would love to just use the bathroom."
+  }
+  else { 
+    console.log(temperature)
+    return "Buggy doesn't know what to think!"
+  }
+  
+}
+
+module.exports = calculateBuggyRespone;
