6e5f02e2539dbe0bef445ab403ada8ea5b329b57
Jmarkus <Jmarkus.fowler@gmail.com>
2024-12-08 17:32:53 -0500

    Updated response output

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let zipCodes = [];
 const submitBtn = document.getElementById("submit-btn");
 const inputEl = document.getElementById("zipcode");
 const zipStore = document.getElementById("storedzip");
+const saysBuggy = document.getElementById("buggysays")
 // const zipcodeFromLocalStorage = JSON.parse(localStorage.getItem("zipCodes"))
 
 // if (zipcodeFromLocalStorage) {
@@ -30,8 +31,9 @@ submitBtn?.addEventListener("click", function () {
   getCoordinates(zipcode)
     .then((coords) => {
       if (coords) {
-        zipStore.innerText = `Lat: ${coords.latitude}, Lon: ${coords.longitude}`;
         getWeather(coords).then((weather) => {
+          const buggyResponse = calculateBuggyResponse(weather.current.temperature_2m,weather.current.weather_code)
+          saysBuggy.innerText = buggyResponse
           console.log(weather);
         });
       } else {
@@ -91,33 +93,37 @@ async function getWeather(coords) {
     throw error;
   }
 }
-function calculateBuggyRespone(temperature, weather_code) {
+function calculateBuggyResponse(temperature, weather_code) {
+  let buggystatement = ""
   if (85 >= temperature && temperature >= 76) {
-    return "Kind of Hot for me! Maybe a short walk or laying down outside.";
+    buggystatement = "Kind of Hot for me! Maybe a short walk or laying down outside.";
   }
   else if (100 >= temperature && temperature >= 86) {
-    return 'It is too hot for me. My paws would get burnt by the pavement, and I might get heat stroke if I am out to long. Water and shade help.'
+    buggystatement = 'It is too hot for me. My paws would get burnt by the pavement, and I might get heat stroke if I am out to long. Water and shade help.'
   }
   else if (75 >= temperature && temperature >= 66) {
-    return "It is nice, but I sure don't want it to get any warmer! Maybe a rest break in 15 to 20."
+    buggystatement = "It is nice, but I sure don't want it to get any warmer! Maybe a rest break in 15 to 20."
   }
   else if (65 >= temperature && temperature >= 50) {
-    return 'Perfect day for a long walk with lots of smells, pets, and treats!!!'
+    buggystatement = 'Perfect day for a long walk with lots of smells, pets, and treats!!!'
   }
   else if (49 >= temperature && temperature >= 30) {
-    return 'Kind of Cold for me! Maybe a medium walk, or a jacket. But we should definitely go get coffee!'
+    buggystatement = 'Kind of Cold for me! Maybe a medium walk, or a jacket. But we should definitely go get coffee!'
   }
   else if (29 >= temperature && temperature >= 15) {
-    return "If it isn't wet out I should be okay for a medium walk. But if it is wet out my paws won't last more than 5 to 10 minutes."
+    buggystatement = "If it isn't wet out I should be okay for a medium walk. But if it is wet out my paws won't last more than 5 to 10 minutes."
   }
   else if (14 >= temperature && temperature >= 0) {
-    return "I would love to just use the bathroom."
+    buggystatement = "I would love to just use the bathroom."
   }
   else { 
-    console.log(temperature)
-    return "Buggy doesn't know what to think!"
+    buggystatement = "Buggy doesn't know what to think!"
   }
-  
+  if (![0,1,2,3,45,48].includes(weather_code)) {
+    buggystatement += " " + "Seems to be some percipitation as well, so probably a shorter than normal walk, unfortunately."
+  }
+  console.log(buggystatement)
+  return buggystatement 
 }
 
-module.exports = calculateBuggyRespone;
+module.exports = calculateBuggyResponse;
